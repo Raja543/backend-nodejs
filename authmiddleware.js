ab56a6553465834e1ee4a2dad7d06487b48dda9c
Raja Kumar <rajakumarmahto952@gmail.com>
2023-08-15 02:43:52 +0530

    updating middleware

--- authmiddleware.js
+++ authmiddleware.js
@@ -2,7 +2,7 @@ function isAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next(); // Move to the next middleware or route handler
     }
-    res.redirect('/login'); // Redirect unauthenticated users to login page
+    res.redirect('/in'); // Redirect unauthenticated users to login page
 }
 
 module.exports = isAuthenticated;
---
+++ next.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const next = () => {
+  return (
+    <div>
+      <h1>My name is raja </h1>
+    </div>
+  )
+}
+
+export default next
